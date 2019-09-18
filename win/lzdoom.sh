c997ef6795dc15c6ea5fcc137e439a04986400c9
Braden Obrzut <admin@maniacsvault.net>
2019-09-18 01:36:53 -0400

    Need to use a different packaging routine for LZDoom since the names changed

--- win/lzdoom.sh
+++ win/lzdoom.sh
@@ -35,6 +35,31 @@ lzdoom_configure() {
 	cmake "$ProjectDir" "${CMakeArgs[@]}"
 }
 
+lzdoom_package() {
+	declare -n Config=$1
+	shift
+	#declare ProjectDir=$1
+	shift
+	declare Version=$1
+	shift
+	declare -n Artifacts=$1
+	shift
+
+	declare Arch
+	for Arch in ${Config[multiarch]}; do
+		(
+			declare DepsDir=$(lookup_build_dir "GZDoom-Deps-$Arch")
+
+			cd "$Arch/Release" &&
+			7z a "../../lzdoom-$Arch-$Version.7z" \
+				./*.exe ./*.pk3 soundfonts/* fm_banks/* \
+				"$DepsDir"/*.dll \
+				-mx=9 &&
+			7z a "../../lzdoom-$Arch-$Version.map.bz2" lzdoom.map -mx=9
+		) &&
+		Artifacts+=("lzdoom-$Arch-$Version.7z" "lzdoom-$Arch-$Version.map.bz2")
+	done
+}
 
 # shellcheck disable=SC2034
 declare -A LZDoomWin=(
@@ -43,7 +68,7 @@ declare -A LZDoomWin=(
 	[configure]=lzdoom_configure
 	[multiarch]='x64 x86'
 	[outoftree]=1
-	[package]=gzdoom_package
+	[package]=lzdoom_package
 	[project]='LZDoom'
 	[remote]='https://github.com/drfrag666/gzdoom.git'
 	[uploaddir]=lzdoom
