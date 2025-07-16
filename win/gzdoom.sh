044f533b6a683147a6685949406c7eb72ed16157
Braden Obrzut <admin@maniacsvault.net>
2025-07-16 02:21:30 -0400

    Update GZDoom dependencies with a test build for ZMusic changes

--- win/gzdoom.sh
+++ win/gzdoom.sh
@@ -60,6 +60,13 @@ gzdoom_package_generic() {
 
 			mapfile -t ExtraFiles < <(find "$DepsDir" -iname '*.dll')
 
+			if [[ $PackageName == 'gzdoom' ]]; then
+				# Replace zmusic.dll with a special build
+				declare ZMusicTestDir
+				ZMusicTestDir=$(lookup_build_dir "GZDoom-ZMusic-Test-$Arch")
+				ExtraFiles=("${ExtraFiles[@]/%*zmusic.dll/$ZMusicTestDir/zmusic.dll}")
+			fi
+
 			cd "$Arch/Release" || return
 
 			if [[ -d fm_banks ]]; then
@@ -124,8 +131,35 @@ declare -A GZDoomDepsWin64=(
 	[outoftree]=0
 	[package]=gzdoom_null
 	[project]='GZDoom-Deps-x64'
-	[remote]='https://github.com/ZDoom/gzdoom/releases/download/g4.13.2/gzdoom-4-13-2-windows.zip'
+	[remote]='https://github.com/ZDoom/gzdoom/releases/download/g4.14.2/gzdoom-4-14-2-windows.zip'
 	[uploaddir]=''
 	[vcs]=DownloadVCS
 )
 register_dep GZDoomDepsWin64
+
+gzdoom_zmusic_test_configure() {
+	declare -n Config=$1
+	shift
+	#declare ProjectDir=$1
+	shift
+	#declare Arch=$1
+	shift
+
+	declare Image=${Config[remote]##*/}
+	7z x "$Image" '*.dll'
+}
+
+# shellcheck disable=SC2034
+declare -A GZDoomZMusicTestWin64=(
+	[branch]=''
+	[build]=gzdoom_null
+	[configure]=gzdoom_zmusic_test_configure
+	[multiarch]='all'
+	[outoftree]=0
+	[package]=gzdoom_null
+	[project]='GZDoom-ZMusic-Test-x64'
+	[remote]='https://github.com/user-attachments/files/21245483/zmusic-dll-2025-07-15.zip'
+	[uploaddir]=''
+	[vcs]=DownloadVCS
+)
+register_dep GZDoomZMusicTestWin64
